8b498095fe1385f53be9921f287deff3cdae4b45
NikolajWeinell <58971957+Weinell@users.noreply.github.com>
2021-04-09 12:41:57 +0200

    IO updated

--- src/IO.java
+++ src/IO.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
 public class IO {
+
+
+    Scanner scanner = new Scanner(System.in);
+    //Team team1 = new Team();
+
+
+    public IO() {
+    }
+
+    public String getUserInput(String msg){
+        System.out.print(msg);
+        Scanner scan = new Scanner(System.in);
+        return scan.nextLine();
+    }
+
+/*
+    public Team createTeams()   {
+
+        System.out.println("Write team name and name of team members ");
+        System.out.println(getUserInput("Team name: "));
+        Team.player1 = getUserInput("Member 1: ");
+        getUserInput("Member 2: ");
+
+
+
+    }
+*/
+
 }
