972820a629fbfc1d4e15c404de0f3dace685c58c
NikolajWeinell <58971957+Weinell@users.noreply.github.com>
2021-04-09 12:41:39 +0200

    Main updated

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,15 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Player player1 = new Player("Magic Mads");
+
+        Team team1 = new Team("Poopers",new Player("Magic"),new Player("Mit navn"));
+        Team team2 = new Team("Pooooopers1");
+
+
+        System.out.println(team1);
+        System.out.println(team2);
+
+
 
 
     }
