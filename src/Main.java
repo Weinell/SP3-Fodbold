3955751a7f6580729aee9cf16e860b41fa458e3b
NikolajWeinell <58971957+Weinell@users.noreply.github.com>
2021-04-09 11:46:39 +0200

    Updated player

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-
+        Player player1 = new Player("Magic Mads");
 
 
     }
--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,35 @@
 public class Player {
 
 
-        //Github test
+    protected String name;
+    protected int ID;
+    static int counter = 0;
+
+
+    public Player(String name)   {
+
+        this.name = name;
+        this.ID = counter;
+        counter++;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
 
 
 }
